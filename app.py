4954503ccaa43aebc47c5312e78d0eb3f5f3379d
aswinxd <musiqorobot@gmail.com>
2025-01-07 22:38:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,4 +105,4 @@ def send_bill():
         return jsonify({"message": "Failed to send bill.", "error": response}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8280)
+    app.run(debug=True, host="0.0.0.0", port=8280)
